5ab237d7d95ef36a2c85a9c1663db61e6e92299b
DottDre <acquarius33@gmail.com>
2024-06-27 12:29:34 +0200

    inizio rotte e struttura progetto

--- erboristeria/src/app/app-routing.module.ts
+++ erboristeria/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'auth',
+    loadChildren:
+    () => import('./auth/auth.module')
+    .then(m => m.AuthModule)
+  },
+    { path: '', loadChildren:
+      () => import('./pages/home/home.module')
+      .then(m => m.HomeModule)
+    },
+    { path: '', loadChildren:
+      () => import('./pages/product/product.module')
+      .then(m => m.ProductModule)
+    },
+    { path: '', loadChildren:
+      () => import('./pages/order/order.module')
+      .then(m => m.OrderModule)
+    }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
